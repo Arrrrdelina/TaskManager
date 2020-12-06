5fc2623bb0c6341b500f22616a8a995203992d78
Adelina Gevorkyan <gevorkyan.adelina@yandex.ru>
2020-12-06 18:03:52 +0300

    Initial commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about'),
+    path('create', views.create, name='create'),
+    path('info', views.info, name='information'),
+    path('sleep', views.sleep, name='sleep')
+]
